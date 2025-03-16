08b28b9da456d86098744a3c7c75f950b1273c1f
ElenaKorneichuk <t.9857294674@gmail.com>
2025-03-16 18:23:32 +0300

    created classes

---
+++ scr/Circle.py
@@ -0,0 +1,18 @@
+from Figure import Figure
+
+class Circle(Figure):
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("Радиус должен быть больше 0")
+        self.radius = radius
+
+    @property
+    def perimeter(self):
+        pi = 3.14159
+        return 2 * pi * self.radius
+
+    @property
+    def area(self):
+        pi = 3.14159
+        return pi * self.radius ** 2
+
